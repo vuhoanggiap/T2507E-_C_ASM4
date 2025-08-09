2557c4a66b5d792e5db5de92d01a04868fc4d291
vuhoanggiap <120465721+vuhoanggiap@users.noreply.github.com>
2025-08-09 22:34:03 +0700

    Add files via upload

---
+++ bai4ASM4.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int n, a = 0, socuoi;
+
+    printf("Nhap so nguyen n: ");
+    scanf("%d", &n);
+
+    int original = n; 
+	
+	if(n < 0){
+		n = n * - 1;
+		while (n > 0) {
+        socuoi = n % 10;          
+        a = a * 10 + socuoi; 
+        n = n / 10;
+		};
+	}else if(n > 0){
+		while (n > 0) {
+        socuoi = n % 10;          
+        a = a * 10 + socuoi; 
+        n = n / 10;
+		};
+	}else{
+		a = 0;
+		printf("%d",a);
+	}
+
+    return 0;
+}
+
